0f51b412c345d5c8bef574777e1a78011b6d0eb8
BeckHarper13 <beckharper13@gmail.com>
2025-02-13 10:56:42 -0800

    created get and post requests

---
+++ server1/main.js
@@ -0,0 +1,25 @@
+document.getElementById('insertButton').addEventListener('click', () => {
+    fetch('http://localhost:3000/insert', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('response').innerText = JSON.stringify(data);
+    });
+});
+
+document.getElementById('queryButton').addEventListener('click', () => {
+    const query = document.getElementById('queryInput').value;
+    const method = query.startsWith('SELECT') ? 'GET' : 'POST';
+    fetch('http://localhost:3000/query', {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('response').innerText = JSON.stringify(data);
+    });
+});
